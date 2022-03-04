591fc4b041f405a93a820d52b66d8963d9f7fed4
Paulius Jankauskas <2578469J@student.gla.ac.uk>
2022-03-04 13:21:43 +0000

    Create groceries models

--- groceries/models.py
+++ groceries/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from food import RawFood
 
-# Create your models here.
+class GroceryItem(models.Model):
+
+    AMOUNT_MAX_LENGTH = 50
+
+    rawFood = models.ForeignKey(RawFood)
+    amount = models.CharField(max_length=AMOUNT_MAX_LENGTH)
+    available = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+
+    EMAIL_MAX_LENGTH = 255
+    PASSWORD_MAX_LENGTH = 255
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    email = models.CharField(max_length=EMAIL_MAX_LENGTH)
+    password = models.CharField(max_length=PASSWORD_MAX_LENGTH)
+
+    def __str__(self):
+        return self.user.username
