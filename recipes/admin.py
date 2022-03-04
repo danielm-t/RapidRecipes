64d45a40043c25b8226fbf8a1e8ca8751a9ed41b
Benjamin Cichos <b.cichos@me.com>
2022-03-04 14:31:41 +0000

    Admin Register Database Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from recipes.models import Category, Recipe, Ingredient, Instruction
+from groceries.models import GroceryItem, UserProfile
+from food.models import Measurement, RawFood
+
+
+admin.site.register(Category)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Instruction)
+admin.site.register(GroceryItem)
+admin.site.register(UserProfile)
+admin.site.register(Measurement)
+admin.site.register(RawFood)
+
 
 # Register your models here.
