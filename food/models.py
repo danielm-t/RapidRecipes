5de6a4eff43ec0a15cb5ccb45735ce2a0c26f5b9
Paulius Jankauskas <2578469J@student.gla.ac.uk>
2022-03-04 13:20:46 +0000

    Create food models

--- food/models.py
+++ food/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Measurement(models.Model):
+
+    TYPE_MAX_LENGTH = 128
+    UNIT_MAX_LENGTH = 128
+    SHORTHAND_MAX_LENGTH = 128
+    ALTERNATIVE_MAX_LENGTH = 255
+
+    class Type(models.TextChoices):
+        TEMPERATURE = 'Temperature'
+        WEIGHT = 'Weight'
+        TIME = 'Time'
+        LENGTH = 'Length'
+        VOLUME = 'Volume'
+    
+    type = models.CharField(max_length=TYPE_MAX_LENGTH, choices = Type.choices)
+    # Name of the unit (Kilogram...)
+    unit = models.CharField(max_length=UNIT_MAX_LENGTH, unique=True)
+    # Shorthand notation (Kg)
+    shorthand = models.CharField(max_length=SHORTHAND_MAX_LENGTH)
+    # Alternative notation (KG.)
+    alternative = models.CharField(max_length=ALTERNATIVE_MAX_LENGTH, blank=True)
+
+    
+class RawFood(models.Model):
+    
+    NAME_MAX_LENGTH = 255
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    measuredIn = models.ManyToManyField(Measurement)
+
+    def __str__(self):
+        return self.name
+
+
