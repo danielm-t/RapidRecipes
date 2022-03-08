e180999b8600eb982b89a1ea22fad593d07ce937
Mihai-Daniel <2569281t@student.gla.ac.uk>
2022-03-08 21:11:08 +0000

    Added the star rating to recepies

--- HTML/script.js
+++ HTML/script.js
@@ -1,3 +1,4 @@
+console.log("aaaa") 
 function searchbar() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
@@ -12,19 +13,4 @@ function searchbar() {
             x[i].style.display="list-item";                 
         }
     }
-}
-const ratings = {
-    hotel_a : 2.8}
-function rating(){
-    const starTotal = 5;
- 
-for(const rating in ratings) {  
-  // 2
-  const starPercentage = (ratings[rating] / starTotal) * 100;
-  // 3
-  const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-  // 4
-  document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded; 
-}
-
 }
