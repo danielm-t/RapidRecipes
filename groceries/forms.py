f7508b5bc7ed871b3459d0d09dd3c18fd67bb25e
Paulius Jankauskas <2578469J@student.gla.ac.uk>
2022-03-10 16:27:56 +0000

    Fixed the previous commit

--- groceries/forms.py
+++ groceries/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 from groceries.models import UserProfile
 
@@ -7,7 +7,3 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture',)
