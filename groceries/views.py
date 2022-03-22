b35d53c3b2ea6803b7c094e9bd0236b60b1a48c0
Benjamin Cichos <b.cichos@me.com>
2022-03-22 14:44:58 +0000

    Grocery List - Can Add an Delete Groceries

--- groceries/views.py
+++ groceries/views.py
@@ -1,13 +1,15 @@
+from calendar import c
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from groceries.models import UserProfile, GroceryItem
+from recipes.models import Measurement
+from food.models import RawFood
 from django.contrib.auth.models import User
 from groceries.forms import UserForm
-from food.models import RawFood
 
 
 def register(request):
@@ -57,3 +59,55 @@ def shopping_list(request):
     context = {'rawFoods' : RawFood.objects.all()}
     context['groceries'] = GroceryItem.objects.filter(user=request.user)
     return render(request, 'groceries/shoppinglist.html', context)
+
+
+def get_added_grocery(request):
+    if request.method == "GET":
+        get = request.GET
+        user = request.user
+        ingredient = get.get("i", None)
+        measurement = get.get("m", None)
+        amount = get.get("a", None)
+        owned = get.get("o", None)
+        if owned:
+            owned = not (owned == "false")
+        else:
+            owned = False
+        context={}
+        try:
+            rawFood = RawFood.objects.get(name=ingredient)
+            measuredIn = Measurement.objects.get(unit=measurement)
+            grocery = GroceryItem.objects.create(rawFood=rawFood,
+                                                        amount=amount,
+                                                        available=owned,
+                                                        user=user,
+                                                        measuredIn=measuredIn)[0]
+            grocery.save()
+            context['groceries'] = GroceryItem.objects.filter(user=request.user)
+        except (RawFood.DoesNotExist, Measurement.DoesNotExist) as e:
+            context['groceries'] = GroceryItem.objects.filter(user=request.user)
+        return render(request, "groceries/table_body.html", context)
+
+def delete_grocery(request):
+    if request.method == "GET":
+        get = request.GET
+        user = request.user
+        ingredient = get.get("i", None)
+        measurement = get.get("m", None)
+        amount = get.get("a", None)
+        owned = get.get("o", None)
+        if owned:
+            owned = not (owned == "false")
+        else:
+            owned = False
+        context = {}
+        try:
+            rawFood = RawFood.objects.get(name=ingredient)
+            measuredIn = Measurement.objects.get(unit=measurement)
+            GroceryItem.objects.filter(rawFood=rawFood, amount=amount, available=owned, user=user, measuredIn=measuredIn).delete()
+            context['groceries'] = GroceryItem.objects.filter(user=request.user)
+            print("hey")
+        except (RawFood.DoesNotExist, Measurement.DoesNotExist) as e:
+            context['groceries'] = GroceryItem.objects.filter(user=request.user)
+        return render(request, "groceries/table_body.html", context)
+
