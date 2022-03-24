c533c496d79855e3d841f3d90c8f2292828f4d37
Cameron Kilgore <2545084k@student.gla.ac.uk>
2022-03-24 17:30:48 +0000

    All filters working

--- recipes/forms.py
+++ recipes/forms.py
@@ -46,6 +46,14 @@ class FilterForm(forms.Form):
     lunch = forms.BooleanField(label='lunch', required=False)
     dinner = forms.BooleanField(label='dinner', required=False)
     breakfast = forms.BooleanField(label='breakfast', required=False)
-
+    easy = forms.BooleanField(label='easy', required=False)
+    medium = forms.BooleanField(label='medium', required=False)
+    hard = forms.BooleanField(label='hard', required=False)
+    drink = forms.BooleanField(label='drink', required=False)
+    vegan = forms.BooleanField(label='vegan', required=False)
+    vegetarian = forms.BooleanField(label='vegetarian', required=False)
+    beef = forms.BooleanField(label='beef', required=False)
+    chicken = forms.BooleanField(label='chicken', required=False)
+    fish = forms.BooleanField(label='fish', required=False)
     
     
--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('<slug:recipe_name_slug>/', views.show_recipe, name='show_recipe'),
+    path('search', views.search, name='search'),
 ]
